fa82e134900d54b20dca4ccbde4364ab3e5f1c0d
rachel <rachelricedev@gmail.com>
2024-07-28 12:21:38 -0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI,
+      )
+  
+      console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
+    }
+  }
+
+//   connection below without mongoose
+// let db,
+//     dbConnectionStr = process.env.DB_STRING,
+//     dbName = 'rescue'
+
+//     async function connectToDatabase() {
+//         try {
+//             const client = await MongoClient.connect(dbConnectionStr);
+//             console.log(`Connected to ${dbName} Database`);
+//             db = client.db(dbName);
+//         } catch (error) {
+//             console.error("Error connecting to database:", error);
+//         }
+//     }
+    
+//     connectToDatabase();
+
+module.exports = connectDB;
